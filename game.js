44e54fc1057fb03cc95a61b66df9e4cb581db298
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-12-14 01:44:09 -0600

    user can select rounds

--- game.js
+++ game.js
@@ -1,18 +1,3 @@
-let choice = ['rock', 'paper', 'scissors'];
-
-// Pick random choice
-function computerPlay() {
-    return choice[Math.floor(Math.random() * 3)];
-}
-
-// let askUserRounds = () => prompt("How many rounds ?")
-
-// Ask user to choose
-let playerSelection = prompt("Rock , Paper , Scissor ?").toLowerCase();
-
-// random choice for computer
-let computerSelection = computerPlay();
-
 // Store Scores
 let playerScore = 0;
 let computerScore = 0;
@@ -26,36 +11,62 @@ let loserScore = () => {
     playerScore ? playerScore -= 1 : 0;
     computerScore++;
 }
-// Main game logic
-function playRound(player, comp) {
-    if (player === comp) {
-        return "Try Again"
-    } else if (player === choice[0] && comp === choice[1]) {
-        loserScore()
-        return "You Lose! Paper wrap Rock"
-    } else if (player === choice[0] && comp === choice[2]) {
-        winnerScore()
-        return "You Win! Rock beats Scissors"
-    } else if (player === choice[1] && comp === choice[0]) {
-        winnerScore()
-        return "You Win! Paper wrap Rock"
-    } else if (player === choice[1] && comp === choice[2]) {
-        loserScore()
-        return "You Lose! Scissors cut Paper"
-    } else if (player === choice[2] && comp === choice[0]) {
-        loserScore()
-        return "You Lose! Rock beats Scissors"
-    } else if (player === choice[2] && comp === choice[1]) {
-        winnerScore()
-        return "You Win! Scissors cut Paper"
+
+let askUserRounds = () => {
+    let rounds = parseInt(prompt("How many rounds ?"));
+    for (let i = 1; i <= rounds; i++) {
+        game()
     }
 }
 
-let final = playRound(playerSelection, computerSelection)
+function game() {
 
-console.log(computerSelection)
-console.log(final)
-console.log(`Your score is ${playerScore}`)
-console.log(`Computer score is ${computerScore}`)
+    let choice = ['rock', 'paper', 'scissors'];
+
+    // Pick random choice
+    function computerPlay() {
+        return choice[Math.floor(Math.random() * 3)];
+    }
+
+    // Assign random choice to computer
+    let computerSelection = computerPlay();
+
+    // Ask user to choose
+    let playerSelection = prompt("Rock , Paper , Scissor ?").toLowerCase();
+
+    function playRound(player, comp) {
+        if (player === comp) {
+            return "Try Again"
+        } else if (player === choice[0] && comp === choice[1]) {
+            loserScore()
+            return "You Lose! Paper wrap Rock"
+        } else if (player === choice[0] && comp === choice[2]) {
+            winnerScore()
+            return "You Win! Rock beats Scissors"
+        } else if (player === choice[1] && comp === choice[0]) {
+            winnerScore()
+            return "You Win! Paper wrap Rock"
+        } else if (player === choice[1] && comp === choice[2]) {
+            loserScore()
+            return "You Lose! Scissors cut Paper"
+        } else if (player === choice[2] && comp === choice[0]) {
+            loserScore()
+            return "You Lose! Rock beats Scissors"
+        } else if (player === choice[2] && comp === choice[1]) {
+            winnerScore()
+            return "You Win! Scissors cut Paper"
+        }
+    }
+    let final = playRound(playerSelection, computerSelection)
+
+    // console interface
+    console.log(computerSelection)
+    console.log(final)
+    console.log(`Your score is ${playerScore}`)
+    console.log(`Computer score is ${computerScore}`)
+}
+
+askUserRounds()
+
 
 
