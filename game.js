e633cfd71dea9d3a7eb185d8058944202ba2caee
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-12-17 22:34:29 -0600

    finised js logic & started styles

--- game.js
+++ game.js
@@ -1,81 +1,112 @@
-let chooseRounds = document.getElementById('rounds');
-let player_score = document.getElementById('player_score');
-let comp_score = document.getElementById('comp_score');
-let finalScore = document.getElementById('final-score');
+let chooseRounds = document.getElementById('input_rounds');
+let playerScore = document.getElementById('player_score');
+let compScore = document.getElementById('comp_score');
+let finalScore = document.getElementById('final_score');
 
-// Player enter no. of game rounds
-chooseRounds.addEventListener("keydown", function (e) {
-    if (e.code === 'Enter')
-        for (let i = 1; i <= chooseRounds.value; i++) {
-            // give time to update and render
-            setTimeout(game, 100);
+let rockBtn = document.getElementById('rock_btn');
+let paperBtn = document.getElementById('paper_btn');
+let scissorsBtn = document.getElementById('scissors_btn');
 
-            clearTimeout()
-        }
-})
+
+let choice = ['rock', 'paper', 'scissors'];
+let playerSelection;
+
+rockBtn.onclick = () => {
+    playerSelection = choice[0]
+    game()
+}
+paperBtn.onclick = () => {
+    playerSelection = choice[1]
+    game()
+}
+scissorsBtn.onclick = () => {
+    playerSelection = choice[2]
+    game()
+}
 
 // Store Scores
-let player = 0;
-let computer = 0;
-
-let winnerScore = () => {
-    player += 1;
-    // computer ? computer -= 1 : 0;
-}
-
-let loserScore = () => {
-    // player ? player -= 1 : 0;
-    computer++;
-}
-
+let pScore = 0;
+let cScore = 0;
 
 function game() {
-    let choice = ['rock', 'paper', 'scissors'];
 
-    // Pick random choice
-    function computerPlay() {
-        return choice[Math.floor(Math.random() * 3)];
-    }
+    // check user selected rounds
+    if (chooseRounds.value > 0) {
 
-    // Assign random choice to computer
-    let computerSelection = computerPlay();
-
-    // Ask user to choose
-    let playerSelection = prompt("Rock , Paper , Scissor ?").toLowerCase();
-
-    function playRound(player, comp) {
-        if (player === comp) {
-            return "Same choice Try Again"
-        } else if (player === choice[0] && comp === choice[1]) {
-            loserScore()
-            return "Paper wrap Rock"
-        } else if (player === choice[0] && comp === choice[2]) {
-            winnerScore()
-            return "Rock beats Scissors"
-        } else if (player === choice[1] && comp === choice[0]) {
-            winnerScore()
-            return "Paper wrap Rock"
-        } else if (player === choice[1] && comp === choice[2]) {
-            loserScore()
-            return "Scissors cut Paper"
-        } else if (player === choice[2] && comp === choice[0]) {
-            loserScore()
-            return "Rock beats Scissors"
-        } else if (player === choice[2] && comp === choice[1]) {
-            winnerScore()
-            return "Scissors cut Paper"
+        // Pick random choice
+        function computerPlay() {
+            return choice[Math.floor(Math.random() * 3)];
         }
 
+        // Assign random choice to computer
+        let computerSelection = computerPlay();
+
+        function playRound(player, comp) {
+            if (player === comp) {
+                return "Same choice Try Again"
+            } else if (player === choice[0] && comp === choice[1]) {
+                cScore++;
+                return "Paper wrap Rock"
+            } else if (player === choice[0] && comp === choice[2]) {
+                pScore++;
+                return "Rock beats Scissors"
+            } else if (player === choice[1] && comp === choice[0]) {
+                pScore++;
+                return "Paper wrap Rock"
+            } else if (player === choice[1] && comp === choice[2]) {
+                cScore++;
+                return "Scissors cut Paper"
+            } else if (player === choice[2] && comp === choice[0]) {
+                cScore++;
+                return "Rock beats Scissors"
+            } else if (player === choice[2] && comp === choice[1]) {
+                pScore++;
+                return "Scissors cut Paper"
+            }
+
+        }
+        let result = playRound(playerSelection, computerSelection)
+        chooseRounds.value--;
+
+        playerScore.textContent = pScore;
+        compScore.textContent = cScore;
+        finalScore.textContent = pScore > cScore ? `WINNER` : pScore === cScore ? `TIE` : `LOSER`;
+
+        console.log(computerSelection)
+        console.log(result)
     }
-    let final = playRound(playerSelection, computerSelection)
-    console.log(computerSelection)
-    player_score.textContent = player;
-    comp_score.textContent = computer;
-    finalScore.textContent = player > computer ? `WINNER` : player === computer ? `TIE` : `LOSER`;
 }
 
 
 
+
+
+// Player enter no. of game rounds
+// chooseRounds.addEventListener("keydown", function (e) {
+//     if (e.code === 'Enter')
+//         for (let i = 1; i <= chooseRounds.value; i++) {
+//             give time to update and render
+//             setTimeout(game, 100);
+//             clearTimeout()
+//         }
+// })
+
+
+        // Ask user to choose
+        // let playerSelection = prompt("Rock , Paper , Scissor ?").toLowerCase();
+
+
+
+
+// let winnerScore = () => {
+//     player++;
+//     computer ? computer -= 1 : 0;
+// }
+
+// let loserScore = () => {
+//     player ? player -= 1 : 0;
+//     computer++;
+// }
 
 
 // askUserRounds()
